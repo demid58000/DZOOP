4e20e0f7ca82dd12e56db830f71dc4a50d0dfb09
demid58000 <demid58000@yandex.ru>
2022-05-24 13:19:05 +0300

    first commit

--- DZOOP.py
+++ DZOOP.py
@@ -10,11 +10,11 @@ class Student:
     def __str__(self):
         return f'''Имя: {self.name}
 Фамилия: {self.surname}
-Средняя оценка за домашние задания:{round(self.avarage_grade(), 1)}
+Средняя оценка за домашние задания:{round(self.average_grade(), 1)}
 Курсы в процессе изучения: {", ".join(self.courses_in_progress)}
 Завершенные курсы:{", ".join(self.finished_courses)}'''
 
-    def avarage_grade(self):
+    def average_grade(self):
         sum_ = 0
         count = 0
         for elem in self.grades.values():
@@ -25,14 +25,18 @@ class Student:
             return 0
         return sum_ / count
 
-    def compare_students(self, student):
-        if isinstance(student, Student):
-            if self.average_grade() < student.average_grade():
-                return f"""{self.name} {self.surname} получает оценки ниже, чем {student.name} {student.surname} """
-            elif self.average_grade() > student.average_grade():
-                return f"""{self.name} {self.surname} получает оценки выше, чем {student.name} {student.surname} """
-            else:
-                return f"""{self.name} {self.surname} получает оценки такие же, как и  {student.name} {student.surname} """
+    # def compare_students(self, student):
+    #   if isinstance(student, Student):
+    #     if self.average_grade() < student.average_grade():
+    #       return f"""{self.name} {self.surname} получает оценки ниже, чем {student.name} {student.surname} """
+    #     elif self.average_grade() > student.average_grade():
+    #       return f"""{self.name} {self.surname} получает оценки выше, чем {student.name} {student.surname} """
+    #     else:
+    #       return f"""{self.name} {self.surname} получает оценки такие же, как и  {student.name} {student.surname} """
+    def __lt__(self, other_student):
+        if isinstance(other_student, Student):
+            return self.average_grade() < other_student.average_grade()
+        return 'Не можем сравнить'
 
     def marks(self, lector, course, mark):
         if isinstance(lector, Lectures) and (course in self.finished_courses \
@@ -60,24 +64,31 @@ class Lectures(Mentor):
     def __str__(self):
         return f'''Имя: {self.name}
 Фамилия: {self.surname}
-Средняя оценка за лекции: {round(self.avarage_grade(), 1)}'''
+Средняя оценка за лекции: {round(self.average_grade(), 1)}'''
 
-    def avarage_grade(self):
+    def __lt__(self, other_lector):
+        if isinstance(other_lector, Lectures):
+            return self.average_grade() < other_lector.average_grade()
+        return 'Не можем сравнить'
+
+    def average_grade(self):
         sum_ = 0
         count = 0
         for elem in self.grades.values():
             sum_ += sum(elem)
             count += len(elem)
-        return sum_ / count
 
-    def compare_lectures(self, lector):
-        if isinstance(lector, Lectures):
-            if self.average_grade() < lector.average_grade():
-                return f"""{self.name} {self.surname} получает оценки ниже, чем {lector.name} {lector.surname} """
-            elif self.average_grade() > lector.average_grade():
-                return f"""{self.name} {self.surname} получает оценки выше, чем {lector.name} {lector.surname} """
-            else:
-                return f"""{self.name} {self.surname} получает оценки такие же, как и  {lector.name} {lector.surname} """
+        if count == 0:
+            return 0
+        return sum_ / count
+    # def compare_lectures(self, lector):
+    #   if isinstance(lector, Lectures):
+    #     if self.average_grade() < lector.average_grade():
+    #       return f"""{self.name} {self.surname} получает оценки ниже, чем {lector.name} {lector.surname} """
+    #     elif self.average_grade() > lector.average_grade():
+    #       return f"""{self.name} {self.surname} получает оценки выше, чем {lector.name} {lector.surname} """
+    #     else:
+    #       return f"""{self.name} {self.surname} получает оценки такие же, как и  {lector.name} {lector.surname} """
 
 
 class Reviewer(Mentor):
@@ -107,22 +118,68 @@ def all_average_grades_students(*students, course_name):
     return round(sum_ / count, 1)
 
 
+def all_average_grades_lectures(*lectures, course_name):
+    sum_ = 0
+    count = 0
+    for lector in lectures:
+        if course_name in lector.courses_attached:
+            sum_ += sum(lector.grades[course_name])
+            count += len(lector.grades[course_name])
+    if count == 0:
+        return 0
+    return round(sum_ / count, 1)
+
+
 student1 = Student('Bob', "Marley", 'man')
 student2 = Student('Bob2', "Marley", 'man')
-all_average_grades_students(student1, student2, course_name="Python")
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Ruoy', 'Eman', 'female')
 best_student.courses_in_progress += ['Python']
+student1.courses_in_progress += ['Java']
+student2.courses_in_progress += ['C++', 'Python']
+student2.finished_courses += ['C']
 
 cool_lector = Lectures('Some', 'Buddy')
 cool_lector.courses_attached += ['Python']
+
 cool_reviever = Reviewer('Bob', 'Salivan')
+cool_reviever.courses_attached += ['Python', 'C++', 'Java']
+
+cool_reviever2 = Reviewer('Matt', 'Salivan')
+cool_reviever2.courses_attached += ['C++', 'Java']
+
+cool_lector2 = Lectures('Bob', 'Marley')
+cool_lector2.courses_attached += ['Java', 'C++']
 
 cool_reviever.rate_hw(best_student, 'Python', 10)
-cool_reviever.rate_hw(best_student, 'Python', 10)
-cool_reviever.rate_hw(best_student, 'Python', 10)
+cool_reviever.rate_hw(student1, 'Python', 10)
+cool_reviever.rate_hw(student2, 'Python', 10)
+
+student1.marks(cool_lector, 'Python', 10)
+best_student.marks(cool_lector, 'Python', 10)
 
 best_student.marks(cool_lector, 'Python', 9)
 print(best_student)
 print(cool_lector)
 print(cool_reviever)
+
+print(cool_reviever)
+print(cool_reviever2)
+print(cool_lector)
+print(cool_lector2)
+print(best_student)
+print(student2)
+print('---------------------------------')
+print(best_student.average_grade(), student2.average_grade())
+print(best_student.__lt__(student2))
+print(best_student < student2)
+print(best_student > student2)
+print('---------------------------------')
+print(best_student < cool_lector)
+print(best_student > cool_lector)
+print('---------------------------------')
+print(cool_lector.average_grade(), cool_lector.average_grade())
+print(cool_lector > cool_lector2)
+
+all_average_grades_students(student1, student2, course_name='Python')
+all_average_grades_lectures(cool_lector, cool_lector2, course_name='Python')
